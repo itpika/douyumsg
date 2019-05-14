a6f78e17d06ef1b56124eb679721723f78eee0c2
dalgurak <dalgurak.liao@outlook.com>
2019-05-14 09:36:51 +0800

    commit

---
+++ client/room.go
@@ -0,0 +1,125 @@
+package client
+
+import (
+	"log"
+	"net"
+	"protocol"
+	"time"
+)
+
+const (
+	addr    = "openbarrage.douyutv.com:8601" // 服务器地址
+	heartbe = "30s"                          // 心跳时间
+)
+
+// room对象可以与服务器建立tcp连接，并与之通信
+type Room struct {
+	RoomId                                  string
+	conn                                    net.Conn
+	login                                   chan bool
+	barrageSwitch, allMsgSwitch, joinSwitch bool
+	barrage, allMsg, join                   chan map[string]string
+	bool
+	logout chan bool
+}
+
+// 返回一个room指针
+func NewRoom(roomId string) *Room {
+	return &Room{RoomId: roomId, login: make(chan bool), logout: make(chan bool)}
+}
+
+// 运行这个room
+func (r *Room) Run() error {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	r.conn = conn
+
+	conn.Write(protocol.MsgToByte(map[string]string{
+		"type":   "loginreq",
+		"roomid": r.RoomId,
+	}))
+	go receiveMsg(r)
+	go r.keepConnection()
+	<-r.logout
+	return nil
+}
+
+// 接收服务器返回的消息
+func receiveMsg(r *Room) {
+	b := make([]byte, 4039)
+	for {
+		n, err := r.conn.Read(b)
+		if err != nil {
+			log.Println("[ERROR]:", err)
+			return
+		}
+		data, err := protocol.ByteToMsg(b[:n])
+		if err != nil {
+			log.Println("[ERROR]:", err)
+			return
+		}
+		switch data["type"] {
+		case "loginres":
+			r.login <- true
+			// 加入组
+			r.conn.Write(protocol.MsgToByte(map[string]string{
+				"type": "joingroup",
+				"rid":  r.RoomId,
+				"gid":  "-9999",
+			}))
+		case "chatmsg":
+			// 弹幕发送
+			if r.barrageSwitch {
+				r.barrage <- data
+			}
+		case "uenter":
+			// 进入放假
+			if r.joinSwitch {
+				r.join <- data
+			}
+
+		default:
+			continue
+		}
+		if r.allMsgSwitch {
+			r.allMsg <- data
+		}
+	}
+}
+
+// 用户进入直播间
+func (r *Room) JoinRoom(chanSize int) <-chan map[string]string {
+	r.joinSwitch = true
+	r.join = make(chan map[string]string, chanSize)
+	return r.join
+}
+
+// 接收弹幕消息
+func (r *Room) ReceiveBarrage(chanSize int) <-chan map[string]string {
+	r.barrageSwitch = true
+	r.barrage = make(chan map[string]string, chanSize)
+	return r.barrage
+}
+
+// 接收所有消息
+func (r *Room) ReceiveAll(chanSize int) <-chan map[string]string {
+	r.allMsgSwitch = true
+	r.allMsg = make(chan map[string]string, chanSize)
+	return r.allMsg
+}
+
+// 客户端与服务器保持连接
+func (r *Room) keepConnection() {
+	<-r.login
+	close(r.login)
+	for {
+		r.conn.Write(protocol.MsgToByte(map[string]string{
+			"type": "mrkl",
+		}))
+		t, _ := time.ParseDuration(heartbe)
+		time.Sleep(t)
+	}
+}
---
+++ protocol/serialize.go
@@ -0,0 +1,119 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"strings"
+)
+
+const (
+	headLen    uint32 = 4
+	msgTypeLen uint32 = 2
+	keepLen    uint32 = 2
+)
+
+// 序列化消息
+func serializeMsg(msg map[string]string) (str string) {
+	for k, v := range msg {
+		str += k + "@=" + v + "/"
+	}
+	str += string(0)
+	return
+}
+
+// 反序列化消息
+func unserializeMsg(str string) map[string]string {
+	m := make(map[string]string)
+	if str[len(str)-1:] != string(0) {
+		return m
+	}
+	// 截取最后的空字符和/
+	str = str[:len(str)-2]
+	slis := strings.Split(str, "/")
+	if len(slis) <= 1 {
+		return m
+	}
+	// 分隔符@=
+	for _, v := range slis {
+		s := strings.Split(v, "@=")
+		if len(s) <= 1 {
+			continue
+		}
+		m[s[0]] = s[1]
+	}
+	return m
+}
+
+// 组合协议
+func combinationProtocolHead(msg map[string]string) []byte {
+	if len(msg) == 0 {
+		return make([]byte, 0)
+	}
+	// 序列化
+	s := serializeMsg(msg)
+	// fmt.Println(s, len(s))
+
+	// 计算头长度
+	head := make([]byte, headLen*2+msgTypeLen+keepLen)
+	// head := make([]byte, 0)
+
+	body := []byte(s)
+
+	// 计算总长度
+
+	totalLen := uint32(len(head) + len(body))
+	// fmt.Println(totalLen)
+	binary.LittleEndian.PutUint32(head, totalLen-headLen)
+	binary.LittleEndian.PutUint32(head[4:], totalLen-headLen)
+	binary.LittleEndian.PutUint16(head[8:], 689)
+	binary.LittleEndian.PutUint16(head[10:], 0)
+	// fmt.Println(head)
+	return append(head, body...)
+}
+
+func MsgToByte(msg map[string]string) []byte {
+	if len(msg) == 0 {
+		return make([]byte, 0)
+	}
+	return combinationProtocolHead(msg)
+}
+
+// 解析数据
+func ByteToMsg(data []byte) (m map[string]string, err error) {
+	if uint32(len(data)) <= headLen*2+msgTypeLen+keepLen {
+		return
+	}
+	reader := bytes.NewReader(data)
+	// fmt.Println(data)
+
+	sli := make([]byte, 4)
+	_, e := reader.ReadAt(sli, 0)
+	if e != nil {
+		err = e
+		return
+	}
+	_, e = reader.ReadAt(sli, 4)
+	if e != nil {
+		err = e
+		return
+	}
+	sli = make([]byte, 2)
+	_, e = reader.ReadAt(sli, 8)
+	if e != nil {
+		err = e
+		return
+	}
+	sli = make([]byte, len(data))
+	n, e := reader.ReadAt(sli, 12)
+	if e != nil && e != io.EOF {
+		err = e
+		return
+	}
+	// fmt.Println(sli[:n])
+	str := string(sli[:n])
+
+	// 反序列化
+	m = unserializeMsg(str)
+	return
+}
